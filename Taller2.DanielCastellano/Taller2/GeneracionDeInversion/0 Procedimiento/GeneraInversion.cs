9577b7f937194b580391c59e887635f5b51a534c
Daniel Castellano <daniel.castellanom@gmail.com>
2016-11-13 17:06:04 -0600

    Paso 1 - Procedimiento Completado

---
+++ Taller2.DanielCastellano/Taller2/GeneracionDeInversion/0 Procedimiento/GeneraInversion.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2.Procedimiento
+{
+    public class GeneraInversion
+    {
+        public static NuevaInversion GeneraNuevaInversion(decimal ValorTransadoNeto,
+            decimal ValorFacial,
+            decimal TasaDeImpuesto,
+            DateTime FechaActual,
+            int PlazoEnDias,
+            bool TratamientoFiscal)
+        {
+            NuevaInversion nuevaInversion = new NuevaInversion();
+
+            nuevaInversion.FechaDeValor = FechaActual;
+
+            DateTime FechaDeVencimiento = FechaActual.AddDays(PlazoEnDias);
+            nuevaInversion.FechaDeVencimiento = FechaDeVencimiento;
+
+            decimal TasaNeta = ((ValorFacial - ValorTransadoNeto) / (ValorTransadoNeto * (PlazoEnDias / 365))) * 100;
+            decimal TasaBruta = TasaNeta / (1 - TasaDeImpuesto);
+            nuevaInversion.TasaBruta = TasaBruta;
+
+            decimal ValorTransadoBruto;
+            if (TratamientoFiscal)
+            {
+                if (DateTime.IsLeapYear(FechaActual.Year))
+                {
+                    ValorTransadoBruto = ValorFacial / (1 + ((TasaBruta) / 100) * PlazoEnDias / 366);
+                }
+                else
+                {
+                    ValorTransadoBruto = ValorFacial / (1 + ((TasaBruta) / 100) * PlazoEnDias / 365);
+                }
+            }
+            else
+            {
+                ValorTransadoBruto = ValorTransadoNeto;
+            }
+            nuevaInversion.ValorTransadoBruto = ValorTransadoBruto;
+
+            decimal ImpuestoPagado;
+            if (TratamientoFiscal)
+            {
+                ImpuestoPagado = Math.Round((ValorTransadoNeto - ValorTransadoBruto), 4);
+            }
+            else
+            {
+                ImpuestoPagado = 0;
+            }
+            nuevaInversion.ImpuestoPagado = ImpuestoPagado;
+
+            decimal RendimientoPorDescuento = Math.Round((ValorFacial - ValorTransadoBruto), 4);
+            nuevaInversion.RendimientoPorDescuento = RendimientoPorDescuento;
+
+            return nuevaInversion;
+
+        }
+    }
+}
---
+++ Taller2.DanielCastellano/Taller2_UnitTests/GeneracionDeInversion/0 Procedimiento/GeneraInversion_Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Taller2.Procedimiento;
+
+namespace Taller2_UnitTests.GeneracionDeInversion.Procedimiento
+{
+    [TestClass]
+    public class GeneraInversion_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
