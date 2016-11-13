491d0c4548b290307d2e3845efa40b233de30296
Daniel Castellano <daniel.castellanom@gmail.com>
2016-11-13 17:30:15 -0600

    Unit Test muestra error. Todas las locales están en 0.

---
+++ Taller2.DanielCastellano/Taller2_UnitTests/GeneracionDeInversion/0 Procedimiento/TasaBruta_Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Taller2.Procedimiento;
+
+namespace Taller2_UnitTests.GeneracionDeInversion.Procedimiento
+{
+    [TestClass]
+    public class TasaBruta_Tests : Escenarios
+    {
+
+        private decimal resultadoEsperado;
+        private decimal resultadoObtenido;
+        private NuevaInversion nuevaInversion;
+
+        [TestMethod]
+        public void TasaBruta_CasoUnico()
+        {
+            resultadoEsperado = 12.3008M;
+            nuevaInversion = NuevaInversionConTratamientoFiscalYAñoBisiesto();
+
+            resultadoObtenido = nuevaInversion.TasaBruta;
+
+            Assert.AreEqual(resultadoEsperado, resultadoObtenido);
+
+
+        }
+    }
+}
