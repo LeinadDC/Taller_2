633e8cb6d24dcad407ca9067714ae26c1f938459
Daniel Castellano <daniel.castellanom@gmail.com>
2016-11-14 16:48:03 -0600

    Paso 4 Con Parameter Object completo

---
+++ Taller2.DanielCastellano/Taller2/GeneracionDeInversion/4 Con Parameter Object/DatosDeLaInversion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2.ParameterObject
+{
+    public class DatosDeLaInversion
+    {
+        public decimal ValorTransadoNeto { get; set; }
+        public decimal ValorFacial { get; set; }
+        public decimal TasaDeImpuesto { get; set; }
+        public DateTime FechaActual { get; set; }
+        public int PlazoEnDias { get; set; }
+        public bool TratamientoFiscal { get; set; }
+    }
+}
