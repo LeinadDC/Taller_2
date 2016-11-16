b3cdacb56f8f1d6d80a4ee667b90770da002343b
Daniel Castellano <daniel.castellanom@gmail.com>
2016-11-16 14:23:21 -0600

    Intento de arreglo

---
+++ Taller2.DanielCastellano/Taller2_UnitTests/GeneracionDeInversion/5 Con Tell Dont Ask/Escenarios.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taller2.TellDontAsk;
+
+namespace Taller2_UnitTests.GeneracionDeInversion.TellDontAsk
+{
+    public class Escenarios
+    {
+        private DatosDeLaInversion losDatos;
+
+        public NuevaInversion NuevaInversionConTratamientoFiscalYAñoBisiesto()
+        {
+            losDatos = new DatosDeLaInversion();
+            losDatos.ValorFacial = 320500;
+            losDatos.ValorTransadoNeto = 300000;
+            losDatos.TasaDeImpuesto = 0.08M;
+            losDatos.FechaActual = new DateTime(2016, 03, 03);
+            losDatos.PlazoEnDias = 221;
+            losDatos.TratamientoFiscal = true;
+
+            return new NuevaInversion(losDatos);
+
+        }
+
+        public NuevaInversion NuevaInversionConTratamientoFiscalYAñoNormal()
+        {
+            losDatos = new DatosDeLaInversion();
+            losDatos.ValorFacial = 320500;
+            losDatos.ValorTransadoNeto = 300000;
+            losDatos.TasaDeImpuesto = 0.08M;
+            losDatos.FechaActual = new DateTime(2014, 03, 03);
+            losDatos.PlazoEnDias = 221;
+            losDatos.TratamientoFiscal = true;
+
+            return new NuevaInversion(losDatos);
+
+        }
+
+        public NuevaInversion NuevaInversionSinTratamientoFiscalYAñoBisiesto()
+        {
+            losDatos = new DatosDeLaInversion();
+            losDatos.ValorFacial = 320500;
+            losDatos.ValorTransadoNeto = 300000;
+            losDatos.TasaDeImpuesto = 0.08M;
+            losDatos.FechaActual = new DateTime(2016, 03, 03);
+            losDatos.PlazoEnDias = 221;
+            losDatos.TratamientoFiscal = false;
+
+            return new NuevaInversion(losDatos);
+
+        }
+
+        public NuevaInversion NuevaInversionSinTratamientoFiscalYAñoNormal()
+        {
+            losDatos = new DatosDeLaInversion();
+            losDatos.ValorFacial = 320500;
+            losDatos.ValorTransadoNeto = 300000;
+            losDatos.TasaDeImpuesto = 0.08M;
+            losDatos.FechaActual = new DateTime(2014, 03, 03);
+            losDatos.PlazoEnDias = 221;
+            losDatos.TratamientoFiscal = false;
+
+            return new NuevaInversion(losDatos);
+
+        }
+    }
+}
---
+++ Taller2.DanielCastellano/Taller2_UnitTests/GeneracionDeInversion/5 Con Tell Dont Ask/ImpuestoPagado.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Taller2.TellDontAsk;
+
+namespace Taller2_UnitTests.GeneracionDeInversion.TellDontAsk
+{
+    [TestClass]
+    public class ImpuestoPagado_Tests : Escenarios
+    {
+
+        private decimal resultadoEsperado;
+        private decimal resultadoObtenido;
+        private NuevaInversion nuevaInversion;
+
+        [TestMethod]
+        public void ImpuestoPagado_AñoBisiesto_ConTratamientoFiscal()
+        {
+            resultadoEsperado = 1659.3592M;
+
+            nuevaInversion = NuevaInversionConTratamientoFiscalYAñoBisiesto();
+            resultadoObtenido = nuevaInversion.ImpuestoPagado;
+
+            Assert.AreEqual(resultadoEsperado, resultadoObtenido);
+        }
+
+        [TestMethod]
+        public void ImpuestoPagado_AñoNormal_ConTratamientoFiscal()
+        {
+            resultadoEsperado = 1659.3592M;
+
+            nuevaInversion = NuevaInversionConTratamientoFiscalYAñoNormal();
+            resultadoObtenido = nuevaInversion.ImpuestoPagado;
+
+            Assert.AreEqual(resultadoEsperado, resultadoObtenido);
+        }
+
+        [TestMethod]
+        public void ImpuestoPagado_AñoBisiesto_SinTratamientoFiscal()
+        {
+            resultadoEsperado = 0;
+
+            nuevaInversion = NuevaInversionSinTratamientoFiscalYAñoBisiesto();
+            resultadoObtenido = nuevaInversion.ImpuestoPagado;
+
+            Assert.AreEqual(resultadoEsperado, resultadoObtenido);
+        }
+
+        [TestMethod]
+        public void ImpuestoPagado_AñoNormal_SinTratamientoFiscal()
+        {
+            resultadoEsperado = 0;
+
+            nuevaInversion = NuevaInversionSinTratamientoFiscalYAñoNormal();
+            resultadoObtenido = nuevaInversion.ImpuestoPagado;
+
+            Assert.AreEqual(resultadoEsperado, resultadoObtenido);
+        }
+    }
+}
